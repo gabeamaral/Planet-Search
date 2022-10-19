a5396e45559255cae50a55621333229f86d87a09
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-10-19 19:42:20 -0300

    Requisito 3 e 4

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,64 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Form() {
-  const { planet, handleClick } = useContext(StarWarsContext);
+  const {
+    planet,
+    handleChange,
+    quantity,
+    quantityForm,
+    getQuantity,
+    getQuantitys,
+    column,
+    columns,
+    Filters,
+  } = useContext(StarWarsContext);
+
   return (
     <main>
       <input
         type="text"
         data-testid="name-filter"
         value={ planet }
-        onChange={ handleClick }
+        onChange={ handleChange }
       />
+      <select
+        data-testid="column-filter"
+        value={ column }
+        onChange={ columns }
+      >
+        {[
+          'population',
+          'orbital_period',
+          'diameter',
+          'rotation_period',
+          'surface_water',
+        ].map((event) => (
+          <option key={ event } value={ event }>
+            {event}
+          </option>
+        ))}
+      </select>
+
+      <select
+        value={ quantityForm }
+        onChange={ getQuantitys }
+        data-testid="comparison-filter"
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual A">igual a</option>
+      </select>
+
+      <input
+        type="number"
+        data-testid="value-filter"
+        value={ quantity }
+        onChange={ getQuantity }
+      />
+
+      <button type="button" data-testid="button-filter" onClick={ Filters }>
+        Adicionar Filtro
+      </button>
     </main>
   );
 }
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -5,23 +5,95 @@ import StarWarsContext from './StarWarsContext';
 function StarWarsProvider({ children }) {
   const [planetsList, setPlanetsList] = useState([]);
   const [planet, setPlanet] = useState('');
+  const [column, setColumn] = useState('population');
+  const [quantity, setQuantity] = useState(0);
+  const [quantityForm, setQuantityForm] = useState('maior que');
+
+  const columns = ({ target: { value } }) => {
+    setColumn(value);
+  };
+
+  const getQuantity = ({ target: { value } }) => {
+    setQuantity(value);
+  };
+
+  const getQuantitys = ({ target: { value } }) => {
+    setQuantityForm(value);
+  };
+
+  const Filters = () => {
+    if (quantityForm === 'maior que') {
+      const data = planetsList.filter((event) => +event[column] > +quantity);
+      setPlanetsList(data);
+    }
+
+    if (quantityForm === 'menor que') {
+      const data = planetsList.filter((event) => +event[column] < +quantity);
+      setPlanetsList(data);
+    }
+
+    if (quantityForm === 'igual A') {
+      const data = planetsList.filter((event) => +event[column] === +quantity);
+      setPlanetsList(data);
+    }
+  };
 
   useEffect(() => {
     const getData = async () => {
       const endpoint = await fetch('https://swapi.dev/api/planets');
       const { results } = await endpoint.json();
-      const data = results.filter((element) => delete element.residents);
+      const data = results.map(
+        ({
+          name,
+          climate,
+          created,
+          diameter,
+          edited,
+          films,
+          gravity,
+          orbital_period: orbital,
+          population,
+          rotation_period: rotation,
+          surface_water: surface,
+          terrain,
+          url,
+        }) => ({
+          name,
+          rotation_period: rotation,
+          surface_water: surface,
+          terrain,
+          url,
+          edited,
+          films,
+          climate,
+          diameter,
+          created,
+          gravity,
+          orbital_period: orbital,
+          population,
+        }),
+      );
       setPlanetsList(data);
     };
     getData();
   }, []);
 
-  const handleClick = ({ target: { value } }) => {
+  const handleChange = ({ target: { value } }) => {
     setPlanet(value);
   };
 
   const planets = useMemo(() => ({
-    planetsList, planet, handleClick }), [planetsList, planet]);
+    planetsList,
+    planet,
+    handleChange,
+    column,
+    columns,
+    quantity,
+    quantityForm,
+    getQuantity,
+    getQuantitys,
+    Filters,
+  }), [planetsList, planet, column, quantity, quantityForm]);
 
   return (
     <StarWarsContext.Provider value={ planets }>
@@ -31,7 +103,7 @@ function StarWarsProvider({ children }) {
 }
 
 StarWarsProvider.propTypes = {
-  children: PropTypes.shape(),
+  children: PropTypes.node,
 }.isRequired;
 
 export default StarWarsProvider;
